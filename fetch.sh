93ebfb798cc2a441d7c1eb75cf482c87cb4b8da8
Helld <mrleesnoi@gmail.com>
2026-04-02 14:55:23 +0300

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,46 +1,30 @@
 #!/bin/bash
 
-blue="\e[1;36m"
+purple="\e[1;35m
 reset="\e[0m"
 
 source /etc/os-release
 arch=$(uname -m)
-echo -e "${blue}OS${reset}: $NAME $arch"
+echo -e "${purple}OS${reset}: $NAME $arch"
 
 vendor=$(cat /sys/class/dmi/id/sys_vendor)
 product=$(cat /sys/class/dmi/id/product_name)
-echo -e "${blue}Host${reset}: $vendor $product"
+echo -e "${purple}Host${reset}: $vendor $product"
 
 krnl=$(uname -r)
-echo -e "${blue}Kernel${reset}: $krnl"
-
-pacman=$(pacman -Qn | wc -l)
-aur=$(yay -Qm | wc -l)
-echo -e "${blue}Packages${reset}: $pacman pacman, $aur AUR"
+echo -e "${purple}Kernel${reset}: $krnl"
 
 term=$(echo $TERM)
-echo -e "${blue}Terminal${reset}: $term"
+echo -e "${purple}Terminal${reset}: $term"
 
 shell=$(ps -p $PPID -o comm=)
-echo -e "${blue}Shell${reset}: $shell"
+echo -e "${purple}Shell${reset}: $shell"
 
 wm=$(echo "$XDG_SESSION_TYPE")
-echo -e "${blue}WM${reset}: $wm"
-
-up=$(uptime -p | sed 's/up //')
-echo -e "${blue}Uptime${reset}: $up"
+echo -e "${purple}WM${reset}: $wm"
 
 install=$(stat -c %w / | cut -d' ' -f1)
-echo -e "${blue}Install Date${reset}: $install"
+echo -e "${purple}Install Date${reset}: $install"
 
 cpu=$(grep -m 1 'model name' /proc/cpuinfo | cut -d: -f2)
-echo -e "${blue}CPU${reset}:$cpu"
-
-gpu=$(lspci | grep -E "VGA|3D" | grep -oP '\[\K[^\]]+(?=\])' | tail -n 1)
-echo -e "${blue}GPU${reset}: $gpu"
-
-memory=$(free -m | awk '/Mem:/{printf "%.1fG / %.1fG (%.0f%%)", $3/1024, $2/1024, $3*100/$2}')
-echo -e "${blue}Mem${reset}: $memory"
-
-disk=$(df -h / | awk 'NR==2 {printf "%s / %s (%s)", $3, $2, $5}')
-echo -e "${blue}Disk (/)${reset}: $disk"
+echo -e "${purple}CPU${reset}:$cpu"
